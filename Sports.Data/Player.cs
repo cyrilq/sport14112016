4e94ec970f9dd5d3959d32932e3988ea1d1f6356
Roma_Ira <student@KIRP-404-STUD04>
2016-11-14 15:39:37 +0300

    Added entity classes

---
+++ Sports.Data/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sports.Data
+{
+    class Player 
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Team Team { get; set; }
+    }
+}
---
+++ Sports.Data/Team.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sports.Data
+{
+    class Team
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+    }
+}
