8abe99692be86f8c37ae287f04f18381cce750c1
Mishdancash <student@KIRP-404-STUD05>
2016-11-14 15:50:30 +0300

    Added Context class

---
+++ Sports.Data/Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sports.Data
+{
+    class Context : DbContext
+    {
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Team> Teams  { get; set; }
+        public DbSet<Coach> Coaches { get; set; }
+
+    }
+}
